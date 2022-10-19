28e39cd9b3dc55dcb6d3d2e978594e0d9b1f5070
Taras <tarashuledza14@gmail.com>
2022-10-19 20:01:00 +0300

    lab 4.3

---
+++ Lab_4.3/Lab_4.3.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double x, xp, xk, dx, F, a, b, c;
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+
+
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+
+	x = xp;
+	while (x <= xk)
+	{
+		if (c < 0 && b != 0)
+			F = a * x * x + b * b * x;
+		else
+			if (c > 0 && b == 0)
+				F = (x + a) / (x + c);
+			else
+				F = x / c;
+
+		cout << "|" << setw(7) << setprecision(2) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+
+
+	return 0;
+}
